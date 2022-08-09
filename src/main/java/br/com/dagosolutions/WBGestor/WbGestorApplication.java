d816c5068e498ca6ea8a6d0e560e483024502f0b
Daniel Santana <danieelsan1y@gmail.com>
2022-08-09 17:56:18 -0300

    frist-commit

---
+++ src/main/java/br/com/dagosolutions/WBGestor/WbGestorApplication.java
@@ -0,0 +1,13 @@
+package br.com.dagosolutions.WBGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dagosolutions/WBGestor/WbGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dagosolutions.WBGestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
